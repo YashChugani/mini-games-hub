84e3f954bdb50001926c1b175f18e5ccc5bcafa3
Yash Chugani <yashchugani494@gmail.com>
2025-03-08 09:50:11 +0530

    Fix routing for Gitub pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import TicTacToe from './components/TicTacToe';
 import RockPaperScissors from './components/RockPaperScissors';
